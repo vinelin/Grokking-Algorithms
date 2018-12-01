31846c530678ff57662b2108850a090e1d8460c9
vinelin <1092968239@qq.com>
2018-12-01 11:10:46 +0800

    chapter 2-3

---
+++ chapter2/selection_sort.py
@@ -0,0 +1,21 @@
+# find the index of the smallest element
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i]<smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        # remove the smallest in arr, and add it in newArr
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(selectionSort([5, 3, 6, 2, 10]))
---
+++ chapter3/find_the_key伪代码.py
@@ -0,0 +1,20 @@
+
+"""
+iterative：
+def look_for_key(main_box):
+    pile = main_box.make_a_pile_to_look_through()
+    while pile is not empty:
+        box = pile.grab_a_box()
+        for item in box:
+            if item.is_a_box():
+                pile.append(item)
+            elif item.is_a_key():
+                print("found the key")
+recursive:
+def look_for_key(box):
+    for item in box:
+        if item.is_a_box():
+            look_for_key(item)
+        elif item.is_a_key():
+             print("found the key")
+"""
